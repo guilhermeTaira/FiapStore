3c9b2d728429e348e681f727dee3e13aa4212188
Guilherme.Taira <gui.s.taira@live.com>
2023-08-25 11:07:04 -0300

    Add project files.

---
+++ FiapStore/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiapStore.Controllers
+{
+    [ApiController]
+    [Route("User")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("get-all-users")]
+        public IActionResult GetAllUsers() 
+        {
+            return Ok("Usuários obtidos com sucesso!");
+        }
+
+        [HttpGet("get-user-by-id")]
+        public IActionResult GetUserById()
+        {
+            return Ok("Usuário obtido com sucesso!");
+        }
+
+        [HttpPost]
+        public IActionResult AddUser()
+        {
+            return Ok("Usuário adicionado com sucesso!");
+        }
+
+        [HttpPut]
+        public IActionResult EditUser()
+        {
+            return Ok("Usuário alterado com sucesso!");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser()
+        {
+            return Ok("Usuário deletado com sucesso!");
+        }
+    }
+}
